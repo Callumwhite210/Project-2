9b3a31ce36fa8cec439495e86e601725021cc834
Himanshu Saxena <contact@hellohimanshu.com>
2020-11-21 11:16:12 +1030

    added express router to render homepage

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// deliver static files in assets folder
+app.use("/public",express.static(path.join(__dirname, "public")));
+
+const routes = require("./controllers/post_controllers");
+
+app.use(routes);
+
+
+app.listen(PORT, ()=> {
+    console.log(`App listening on PORT: ${PORT}`);
+});
