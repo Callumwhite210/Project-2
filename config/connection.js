8e295e652dd816fefaca17fcd68362e5243a6a08
Callum White <callumwhite210@gmail.com>
2020-11-25 15:11:57 +1030

    change connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,32 @@
 const Sequelize = require("sequelize");
+const { connect } = require("../controllers/post_controllers");
 
-let sequelize = new Sequelize("posts_db", "root", "localpass", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  sequelize = new Sequelize("posts_db", "root", "localpass", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  })
+};
+
+connect.connect();
+
+ /* let sequelize = new Sequelize("posts_db", "root", "localpass", {
+   host: "localhost",
+   port: 3306,
+   dialect: "mysql",
+   pool: {
+     max: 5,
+     min: 0,
+     idle: 10000
+   }
+ }); */
 
 module.exports = sequelize;
