6d70672d98374c876f248b6d30ce3c7a5a7148ea
Callum White <callumwhite210@gmail.com>
2020-11-25 15:28:09 +1030

    fix connect

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,15 @@ const { connect } = require("../controllers/post_controllers");
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
-  sequelize = new Sequelize("posts_db", "root", "localpass", {
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "localpass",
+      database: "posts_db"
+    })
     }
-  })
-};
+
 
 connect.connect();
 
